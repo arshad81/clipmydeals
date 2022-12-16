6ecede0d7c578a4f534866f30d48fc11ca5fd053
arshad <arshad@sellergize.com>
2022-12-16 15:46:56 +0530

    Mobile View Done

--- index.php
+++ index.php
@@ -12,12 +12,12 @@ include "./header.php"; ?>
             <img src="./assets/images/Lg-Banner-1.jpg" class="d-none d-lg-block w-100 " alt="...">
             <div class="position-absolute top-0 start-50 translate-middle-x  w-100">
                 <p class="h1 text-custom-white text-center mx-auto mt-4 lh-sm carousel-text">The Most comprehensive affiliate theme for Wordpress</h1>
-                <div class="d-flex justify-content-center mt-3">
-                    <button class="btn btn-outline-light mx-2"> <img class="me-1" src="./assets/images/icons/btn-video.svg" alt="buy-now" width="14px"> Watch Video</button>
+                <div class="d-flex justify-content-center mt-10p">
+                    <button class="btn btn-outline-light mx-2 watch-btn"> <img class="me-1 " src="./assets/images/icons/btn-video.svg" alt="buy-now" width="14px"> Watch Video</button>
                     <button class="btn btn-primary mx-2"> <img class="me-1" src="./assets/images/icons/buy-now-icon.svg" alt="buy-now" width="14px"> Buy Now</button>
                 </div>
                 <div>
-                    <div class="d-flex justify-content-evenly mt-3">
+                    <div class="d-flex justify-content-center mt-3">
                         <div class="glassmorphism-text-div mx-1  d-flex py-1 align-items-center">
                             <img class="mx-1" src="./assets/images/icons/banner-android-icon.svg" width="24px" alt="">
                             <p class="text-light m-0 mx-1  fs-6">FREE Android App (White Labelled App worth ₹4000 for your website) </p>
@@ -35,14 +35,14 @@ include "./header.php"; ?>
         <img src="./assets/images/mobile-version-images-2.jpg" class="d-block d-lg-none w-100 opacity-100" width="360px" alt="...">
             <img src="./assets/images/Lg-Banner-1.jpg" class="d-none d-lg-block w-100 " alt="...">
             <div class="position-absolute top-0 start-50 translate-middle-x  w-100">
-                <p class="h1 text-custom-white text-center mx-auto mt-4 lh-sm carousel-text">The Most comprehensive affiliate theme for Wordpress</h1>
-                <div class="d-flex justify-content-center mt-3">
+                <p class="h1 text-custom-white text-center mx-auto mt-4 lh-sm carousel-text">High featured android application specially made for you!</h1>
+                <div class="d-flex justify-content-center mt-10p">
                     <button class="btn btn-outline-light mx-2"> <img class="me-1" src="./assets/images/icons/btn-video.svg" alt="buy-now" width="14px"> Watch Video</button>
                     <button class="btn btn-primary mx-2"> <img class="me-1" src="./assets/images/icons/buy-now-icon.svg" alt="buy-now" width="14px"> Buy Now</button>
                 </div>
                 <div>
-                    <div class="d-flex justify-content-evenly mt-3">
-                        <div class="glassmorphism-text-div mx-1  d-flex py-1 align-items-center">
+                    <div class="d-flex justify-content-center mt-3">
+                        <div class="glassmorphism-text-div border border-2  border-primary mx-1  d-flex py-1 align-items-center">
                             <img class="mx-1" src="./assets/images/icons/banner-android-icon.svg" width="24px" alt="">
                             <p class="text-light m-0 mx-1  fs-6">FREE Android App (White Labelled App worth ₹4000 for your website) </p>
                         </div>
@@ -59,18 +59,18 @@ include "./header.php"; ?>
         <img src="./assets/images/mobile-version-images-3.jpg" class="d-block d-lg-none w-100 opacity-100" width="360px" alt="...">
             <img src="./assets/images/Lg-Banner-1.jpg" class="d-none d-lg-block w-100 " alt="...">
             <div class="position-absolute top-0 start-50 translate-middle-x  w-100">
-                <p class="h1 text-custom-white text-center mx-auto mt-4 lh-sm carousel-text">The Most comprehensive affiliate theme for Wordpress</h1>
-                <div class="d-flex justify-content-center mt-3">
+                <p class="h1 text-custom-white text-center mx-auto mt-4 lh-sm carousel-text">Single API to get Coupons from all Affiliate Programs</h1>
+                <div class="d-flex justify-content-center mt-10p">
                     <button class="btn btn-outline-light mx-2"> <img class="me-1" src="./assets/images/icons/btn-video.svg" alt="buy-now" width="14px"> Watch Video</button>
                     <button class="btn btn-primary mx-2"> <img class="me-1" src="./assets/images/icons/buy-now-icon.svg" alt="buy-now" width="14px"> Buy Now</button>
                 </div>
                 <div>
-                    <div class="d-flex justify-content-evenly mt-3">
+                    <div class="d-flex justify-content-center mt-3">
                         <div class="glassmorphism-text-div mx-1  d-flex py-1 align-items-center">
                             <img class="mx-1" src="./assets/images/icons/banner-android-icon.svg" width="24px" alt="">
                             <p class="text-light m-0 mx-1  fs-6">FREE Android App (White Labelled App worth ₹4000 for your website) </p>
                         </div>
-                        <div class="glassmorphism-text-div mx-1 py-1 d-flex align-items-center">
+                        <div class="glassmorphism-text-div border border-2  border-primary mx-1 py-1 d-flex align-items-center">
                             <img class="mx-1" src="./assets/images/icons/banner-android-icon.svg" width="24px" alt="">
                             <p class="text-light m-0 mx-1  fs-6">FREE CouponAPI Voucher (Get ₹2200 OFF on couponapi.org) </p>
                         </div>
